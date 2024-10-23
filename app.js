17afaebaa76a56da4e67429ae159cbb80dabefb8
Sambhav Jain <151374631+sambhavjain321@users.noreply.github.com>
2024-10-23 04:53:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+let counters = document.querySelectorAll('.counter');
+let time = 300;
+
+counters.forEach(counter => {
+  let updateCount = () => {
+    let target = +counter.getAttribute('data-target');
+    let count = +counter.innerText;
+
+    let increment = target / time;
+
+    if (count < target) {
+      counter.innerText = Math.ceil(count + increment);
+      setTimeout(updateCount, 300);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+
+});
+
+let healthCondition = document.querySelector('.health');
+
+setTimeout(() => {
+  healthCondition.innerText = "You are Awesome!"
+}, 20000);
